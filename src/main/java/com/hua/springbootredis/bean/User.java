25c79cac48ad77b9fb7b0ae596fcaab576b44d76
hua77 <15979695011@163.com>
2018-08-17 14:37:02 +0800

    添加user用例

---
+++ src/main/java/com/hua/springbootredis/bean/User.java
@@ -0,0 +1,36 @@
+package com.hua.springbootredis.bean;
+/*
+创建用例user  by zyh
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
